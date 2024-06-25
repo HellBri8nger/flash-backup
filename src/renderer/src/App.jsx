91e24090abf9c31c043bcdcd9d39b0c71b120b89
Lupin <theybetrayedme1@gmail.com>
2024-06-25 16:19:28 +0530

    feat: add: added mockup ui refactor: removed boilerplate code and some modules

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,35 +1,30 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import "./assets/app.scss"
+import { Button, Modal, Select, TextInput } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+
+const backup_services = ['Local', 'Google Drive', 'Dropbox', 'Mega', 'OneDrive']
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+  const [opened, { open, close }] = useDisclosure(false);
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <div className="mainbar">
+      <Modal opened={opened} onClose={close} title="Add Game" className="addGameModal">
+        <TextInput label="Name" placeholder="Input Name of The Game" />
+        <TextInput label="Path" placeholder="Input Path of Game" />
+        <Button>Add Game</Button>
+      </Modal>
+
+      <Select
+        placeholder="Select Backup Service"
+        data={backup_services}
+        className="serviceDropdown"
+        searchable="true"
+        required="true"
+      />
+      <Button onClick={open}>Add New Game</Button>
+    </div>
   )
 }
 
 export default App
-
