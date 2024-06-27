3b133502373f951d92cd857e4f39d139203561a0
Lupin <theybetrayedme1@gmail.com>
2024-06-27 16:59:03 +0530

    feat: add: added dialog to select folder

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,17 +1,32 @@
 import "./assets/app.scss"
 import { Button, Modal, Select, TextInput } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
+import { useState } from 'react'
 
 const backup_services = ['Local', 'Google Drive', 'Dropbox', 'Mega', 'OneDrive']
 
 function App() {
   const [opened, { open, close }] = useDisclosure(false);
+  const [pathValue, setPathValue] = useState('');
+
+  const handleFolder = async () => {
+    const folderPath = await window.electronAPI.folder()
+    setPathValue(folderPath)
+  }
 
   return (
     <div className="mainbar">
       <Modal opened={opened} onClose={close} title="Add Game" className="addGameModal">
-        <TextInput label="Name" placeholder="Input Name of The Game" />
-        <TextInput label="Path" placeholder="Input Path of Game" />
+        <TextInput label="Name" placeholder="Input Name" />
+        <div className="pathSelector">
+          <TextInput
+            label="Path"
+            placeholder="Input Save Location"
+            value={pathValue}
+            onChange={e => setPathValue(e.target.value)}
+          />
+          <Button onClick={handleFolder}>Select Folder</Button>
+        </div>
         <Button>Add Game</Button>
       </Modal>
 
@@ -20,7 +35,7 @@ function App() {
         data={backup_services}
         className="serviceDropdown"
         searchable="true"
-        required="true"
+        required={true}
       />
       <Button onClick={open}>Add New Game</Button>
     </div>
@@ -28,3 +43,4 @@ function App() {
 }
 
 export default App
+
