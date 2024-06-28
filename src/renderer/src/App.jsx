d8e483ddf8065d9ab60320267304605c8e3cf5f4
JoelJoshy2004 <130413550+JoelJoshy2004@users.noreply.github.com>
2024-06-28 11:07:57 +1000

    Added launch ui

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,18 +1,25 @@
-import "./assets/app.scss"
-import { Button, Modal, Select, TextInput } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
-import { useState } from 'react'
+import './assets/app.scss';
+import { Button, Modal, Select, TextInput } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
+import Launch from './launch';
 
-const backup_services = ['Local', 'Google Drive', 'Dropbox', 'Mega', 'OneDrive']
+const backup_services = ['Local', 'Google Drive', 'Dropbox', 'Mega', 'OneDrive'];
 
 function App() {
   const [opened, { open, close }] = useDisclosure(false);
+  const [launchOpened, { open: openLaunch, close: closeLaunch }] = useDisclosure(false);
   const [pathValue, setPathValue] = useState('');
 
   const handleFolder = async () => {
-    const folderPath = await window.electronAPI.folder()
-    setPathValue(folderPath)
-  }
+    const folderPath = await window.electronAPI.folder();
+    setPathValue(folderPath);
+  };
+
+  const handleAddGame = () => {
+    close();
+    openLaunch();
+  };
 
   return (
     <div className="mainbar">
@@ -23,24 +30,25 @@ function App() {
             label="Path"
             placeholder="Input Save Location"
             value={pathValue}
-            onChange={e => setPathValue(e.target.value)}
+            onChange={(e) => setPathValue(e.target.value)}
           />
           <Button onClick={handleFolder}>Select Folder</Button>
         </div>
-        <Button>Add Game</Button>
+        <Button onClick={handleAddGame}>Add Game</Button>
       </Modal>
 
       <Select
         placeholder="Select Backup Service"
         data={backup_services}
         className="serviceDropdown"
-        searchable="true"
-        required={true}
+        searchable
+        required
       />
       <Button onClick={open}>Add New Game</Button>
+      
+      <Launch opened={launchOpened} close={closeLaunch} />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
